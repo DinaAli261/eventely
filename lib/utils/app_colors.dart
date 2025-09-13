4d977375958a92106a762b52ae9a3095aac6f3eb
dina ali <alidina275@gamil.com>
2025-09-13 02:42:13 +0200

    +initialize project

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color blue = Color(0xFF5669FF);
+  static const Color red = Color(0xFFFF5659);
+  static const Color grey = Color(0xFF7B7B7B);
+  static const Color black = Color(0xFF1C1C1C);
+  static const Color darkBlue = Color(0xFF101127);
+  static const Color lightBlue = Color(0xFF5669FF);
+  static const Color offWhite = Color(0xFFF4EBDC);
+  static const Color white = Color(0xFFF2FEFF);
+}
